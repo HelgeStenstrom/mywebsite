f893a77014143c97308ca058a8397b1b5cca63f6
Helge Stenström <h.stenstrom@gmail.com>
2022-10-30 16:18:08 +0100

    Express server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hej");
+const express = require("express");
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res
+        //.status(404)
+        .json({meddelande: "fel"})
+
+    //    .send("Hej")
+    ;
+    //res.sendStatus(500);
+    let seven = req.rawHeaders[7];
+    console.log('Here  from ', seven);
+});
+
+app.listen(3000);
