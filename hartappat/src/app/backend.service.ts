d386b22df36c42358e5d5cef918de72860499d5d
Helge Stenström <h.stenstrom@gmail.com>
2022-11-13 22:18:35 +0100

    Error handling, rudimentär

--- hartappat/src/app/backend.service.ts
+++ hartappat/src/app/backend.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,9 +26,27 @@ export class BackendService {
    getGrapes(): Observable<Grape[]> {
     console.log('BackendService.getData() called');
      const url: string = this.urlBase + 'grapes';
-     return this.http.get<Grape[]>(url);
+     return this.http
+       .get<Grape[]>(url)
+       .pipe(
+         catchError(this.handleError)
+       )
+       ;
   }
 
+  private handleError(error: HttpErrorResponse) { // From https://angular.io/guide/http#getting-error-details
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 
 }
 
