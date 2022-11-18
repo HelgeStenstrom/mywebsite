a0b3acc5117c49266f29c239417813cb4da48650
Helge Stenström <h.stenstrom@gmail.com>
2022-11-18 21:14:21 +0100

    Backend: Lägga till druvor fungerar, men dåligt. Jag vet inte vad metoderna returnerar. Detektering av fel (t.ex. en färg som inte finns) fungerar inte.

--- startMariaDb.js
+++ startMariaDb.js
@@ -65,6 +65,83 @@ app.get('/grapes', async (req, res) => {
     }
 });
 
+function postGrapeHandler() {
+    // TODO: Check POST for code at work
+    return (req, res) => {
+        //console.log("postGrapeHandler called", req.query);
+        try {
+            insertGrape(req.query.name, req.query.color);
+            res.status(201).send("postGrapeHandler called");
+        } catch (e) {
+            res.status(418).send("unacceptable Wine/color combo");
+        }
+        // //console.log("result: ", res);
+        // if (res.sdfsd) {
+        //     res.status(201).send("postGrapeHandler called");
+        // } else {
+        //     res.status(418).send("unacceptable Wine/color combo");
+        // }
+    };
+}
+
+async function insertGrape(grapeName, grapeColor) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const sql = "insert into hartappat.grapes (name, color) value (?, ?);"
+        const res = await conn.query(sql, [grapeName, grapeColor]);
+
+        console.log("InsertGrape resonse: ",  res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+        return res;
+
+    } catch (err) {
+        console.error(err);
+        throw err;
+    } finally {
+        if (conn) {
+            conn.end();
+        }
+    }
+}
+
+app.post('/g2',  postGrapeHandler());
+
+app.post('/grapes', async (req, res) => {
+    //res.send("PUT Request Called")
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const grapeName = 'Name';
+        const grapeColor = 'green';
+        const sql = `insert into hartappat.grapes (name, color) value (${grapeName}, ${grapeColor});`;
+
+        let promise = conn.query(sql);
+
+        promise.then((x) => res.json(x));
+        await conn.end();
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+app.delete('/grapes/:id', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        let sql = `
+            select w.name, w.color, d.sv as farg
+            from hartappat.grapes w
+                     join hartappat.dictionary d
+                          on w.color = d.en;
+        `;
+        let promise = conn.query(sql);
+        promise.then((x) => res.json(x));
+        await conn.end();
+    } catch (e) {
+        console.error(e);
+    }
+});
+
 app.get('/countries', async (req, res) => {
     let conn;
     try {
