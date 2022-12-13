eec6f8d6008d085bbfac9eff2e3dd8be98073961
Helge Stenström <h.stenstrom@gmail.com>
2022-12-13 21:52:01 +0100

    Dropzon funkar lite. Ingen större nytta ännu.

--- hartappat/src/app/filedrop/filedrop.component.ts
+++ hartappat/src/app/filedrop/filedrop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-filedrop',
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filedrop.component.css']
 })
 export class FiledropComponent implements OnInit {
+  private fileOver = false;
 
-  constructor() { }
+  constructor() {
+    // Nothing to do yet
+  }
 
   ngOnInit(): void {
+    // Nothing to do yet
+  }
+
+  // Dragover listener
+  @HostListener('dragover', ['$event'])
+  onDragOver(evt: { preventDefault: () => void; stopPropagation: () => void; }) {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    console.log('Drag over');
+  }
+
+  // Dragleave listener
+  @HostListener('dragleave', ['$event'])
+  public onDragLeave(evt: { preventDefault: () => void; stopPropagation: () => void; }) {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    console.log('Drag leave');
+  }
+
+  // Drop listener
+  @HostListener('drop', ['$event'])
+  public ondrop(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+    const files: FileList = evt.dataTransfer.files;
+    if (files.length > 0) {
+      // Do some stuff here
+      console.log(`You dropped ${files.length} file(s).`);
+      console.log(files);
+      for (let i = 0; i < files.length; i++) { // NOSONAR
+        const file = files[i];
+        console.log(file.name);
+        console.log(file.webkitRelativePath);
+        console.log(file.type);
+        console.log(file.size);
+        // It's unclear to me how we can find the actual file, since we only have the name, not the full path.
+        // And it's in the client machine, not in the browser.
+      }
+    }
   }
 
 }
