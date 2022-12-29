b150578ad59fd752372dc342d9f7cf974f894563
Helge Stenström <h.stenstrom@gmail.com>
2022-12-30 00:26:56 +0100

    Refactoring av testmetod

--- src/__tests__/SomeTest.test.ts
+++ src/__tests__/SomeTest.test.ts
@@ -3,45 +3,41 @@ import {agent as request} from "supertest";
 import {app} from "../backend";
 
 describe('real backend', () => {
+    type closable =  { close: () => void; };
 
-    let connection;
+    let connection: closable;
+    let testAgent;
     beforeEach(() => {
-        connection = app.listen(3003, () => {
-        });
+        connection = app.listen(3003);
+        testAgent = request(connection);
     });
 
     afterEach(() => {
         connection.close();
     });
 
-    test('it returns 200 OK, checked by Supertest', (done) => {
-        request(connection)
+
+    test('it returns 200 OK, checked by Supertest', async () => {
+        await testAgent
             .get('/api/v1/vinprovning/13')
-            .expect(200, done);
+            .expect(200);
     });
 
-    test('it returns 200 OK, checked by Jest', (done) => {
-        request(connection)
-            .get('/api/v1/vinprovning/13')
-            .then((response) => {
-                expect(response.status).toBe(200);
-                done();
-            });
+    test('it returns 200 OK, checked by Jest', async () => {
+        const response = await testAgent.get('/api/v1/vinprovning/13');
+        expect(response.status).toBe(200);
     });
 
-    test('it returns the requested id', (done) => {
-        request(connection)
-            .get('/api/v1/vinprovning/14')
-            .then((response) => {
-                expect(response.body.id).toBe(14);
-                done();
-            });
+    test('it returns the requested id', async () => {
+        const response = await testAgent
+            .get('/api/v1/vinprovning/14');
+        expect(response.body.id).toBe(14);
     });
 
-    test('it returns 404 when the id is invalid', (done) => {
-        request(connection)
+    test('it returns 404 when the id is invalid', async () => {
+        testAgent
             .get('/api/v1/vinprovning/xyz')
-            .expect(404, done);
+            .expect(404);
     });
 
     // TODO: connect to a testing database, maybe mocked
