5122c2c7e68b96d37bc62909a5c540193780a40c
Helge Stenström <h.stenstrom@gmail.com>
2023-01-06 14:24:26 +0100

    Wikipedia: Zero-pad month

--- src/externalEndpointProxy.ts
+++ src/externalEndpointProxy.ts
@@ -37,7 +37,7 @@ app.get('/featured', (req, res) => {
         const year = today.getFullYear();
         const month = today.getMonth() + 1;
         const date = today.getDate();
-        const url = `https://sv.wikipedia.org/api/rest_v1/feed/featured/${year}/${month}/${zeroPad(date, 2)}`;
+        const url = `https://sv.wikipedia.org/api/rest_v1/feed/featured/${year}/${zeroPad(month, 2)}/${zeroPad(date, 2)}`;
         console.log("EP: Idag är det ", today);
         console.log("EP YMD: ", year, month, date);
         return url;
