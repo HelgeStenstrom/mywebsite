1b821ad686b01618e4edc2ac8d3d95c582b4e8b1
Helge Stenström <h.stenstrom@gmail.com>
2023-01-09 21:47:49 +0100

    Försök att testa BackendService. Några länkar att läsa innan jag går vidare.

--- hartappat/src/app/backend.service.spec.ts
+++ hartappat/src/app/backend.service.spec.ts
@@ -2,16 +2,38 @@ import {TestBed} from '@angular/core/testing';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 import {BackendService, Grape} from './backend.service';
+import {HttpTestingController} from "@angular/common/http/testing";
 
 // Based on https://stackoverflow.com/questions/59204306/trying-to-run-angular-httpclient-jasmine-test-against-live-rest-api-nothing-hap
-xdescribe('BackendService with mocked backend (faking MariaDB)', () => {
+describe('BackendService with mocked backend (faking MariaDB)', () => {
 
   let service: BackendService;
-  let mockedHtttpClient: HttpClient;
+
+  // Läs mera https://stackoverflow.com/questions/46930581/how-to-mock-httpclient-in-a-provided-service-in-a-component-test-in-angular
+  // https://medium.com/netscape/testing-with-the-angular-httpclient-api-648203820712
+  // https://angular.io/guide/http#testing-http-requests
+  // https://www.thecodebuzz.com/angular-unit-test-and-mock-httpclient-get-request/
+  // https://ng-mocks.sudo.eu/guides/http-request
+
+  const httpClientStub: Partial<HttpClient> = {
+/*
+    get(url: string): Observable<Grape[]> {
+      const aGrape: Grape = {name: 'Riesling?', color:'grön'};
+      const someGrapes: Grape[] = [aGrape];
+      const observable: Observable<Grape[]> = of(someGrapes);
+      return observable;
+    }
+*/
+  };
+
+  const httpMock: HttpTestingController = TestBed.get(
+    HttpTestingController,
+  );
 
   beforeEach(() => {
     TestBed.configureTestingModule({
 
+      providers: [{provide: HttpClient, useValue: httpClientStub}],
     });
     // TODO: Read about mocking HttpClient
     service = TestBed.inject(BackendService);
@@ -32,6 +54,7 @@ xdescribe('BackendService with mocked backend (faking MariaDB)', () => {
 xdescribe('BackendService Test with active backend (MariaDB)', () => {
   let httpClient: HttpClient;
   let service: BackendService;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
