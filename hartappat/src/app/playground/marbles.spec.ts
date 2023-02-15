2e0873e12c3000a957715c440e4f9ead6ea7c13f
Helge Stenström <h.stenstrom@gmail.com>
2023-02-15 22:28:12 +0100

    Marble testing of take(2) doesn't work yet.

--- hartappat/src/app/playground/marbles.spec.ts
+++ hartappat/src/app/playground/marbles.spec.ts
@@ -1,5 +1,5 @@
 import {TestScheduler} from "rxjs/testing";
-import {map} from "rxjs/operators";
+import {map, share, take} from "rxjs/operators";
 
 describe('First a pipe-cleaner!', () => {
 
@@ -13,10 +13,11 @@ describe('Marble test with RxJs testing utils', () => {
   let scheduler: TestScheduler;
 
   beforeEach(() => {
+    //scheduler = new TestScheduler((actual, expected) => expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected)));
     scheduler = new TestScheduler((actual, expected) => expect(actual).toEqual(expected));
   });
 
-  it('should assert two variables with the RxJs testing utils', () => {
+  it('should assert two variables with the RxJs testing utilsk', () => {
     scheduler.run(({cold, expectObservable}) => {
       const sourceValues = {a: 1, b: 2, c: 3};
       const source$ = cold('a-b--c--|', sourceValues);
@@ -29,4 +30,34 @@ describe('Marble test with RxJs testing utils', () => {
 
     });
   });
+
+
+  xit('should show how take works', () => {
+
+    scheduler.run(({cold, expectObservable}) => {
+
+      const sourceValues = {a: 1, b: 7, c: 3, d: 4};
+      const source$ = cold('a-b--c--d-|', sourceValues);
+
+      let expectedValues = {a: 1, b: 7};
+      expectedValues = sourceValues;
+      const expected$ = cold('a-b--c--d|', expectedValues);
+
+      const result$ = source$.pipe(take(4));
+      expectObservable(result$).toEqual(expected$)
+
+    });
+
+  });
+
+  xit('test', () => {
+    scheduler.run((helpers) => {
+      const { cold, expectObservable } = helpers;
+      const source$ = cold('-a-b-c|');
+      const expected = '-a-b-c|';
+
+      expectObservable(source$.pipe(take(3))).toBe(expected);
+    });
+  });
+
 });
