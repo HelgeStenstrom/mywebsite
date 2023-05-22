5c7a956bea74e4e144215502d36312f2ece62e96
Helge Stenström <h.stenstrom@gmail.com>
2023-05-22 21:30:33 +0200

    Lite unit-testande av Wine-komponenten. Tyvärr finns ingen garanti att rätt komponenter har markerats med namn.

--- hartappat/src/app/components/wine/wine.component.spec.ts
+++ hartappat/src/app/components/wine/wine.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WineComponent } from './wine.component';
+import {By} from "@angular/platform-browser";
+
+// Inspired by https://youtu.be/uefGmRcIm3c
+// What building with TDD actually looks like
 
 describe('WineComponent', () => {
   let component: WineComponent;
@@ -18,4 +22,41 @@ describe('WineComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an Add button', () => {
+    const debugElement = fixture.debugElement.query(
+      By.css('[data-test="add-button"]'));
+    expect(debugElement).toBeTruthy();
+  });
+
+  it('should have a Name input', () => {
+    const debugElement = fixture.debugElement.query(
+      By.css('[data-test="name-input"]'));
+    expect(debugElement).toBeTruthy();
+  });
+
+  it('should have a Country selector', () => {
+    const debugElement = fixture.debugElement.query(
+      By.css('[data-test="country-input"]'));
+    expect(debugElement).toBeTruthy();
+  });
+
+  it('should have a Type selector', () => {
+    const debugElement = fixture.debugElement.query(
+      By.css('[data-test="type-input"]'));
+    expect(debugElement).toBeTruthy();
+  });
+
+  it('should have a Systembolaget input', () => {
+    const debugElement = fixture.debugElement.query(
+      By.css('[data-test="systembolaget-input"]'));
+    expect(debugElement).toBeTruthy();
+  });
+
+  it('should return a wine object', () => {
+
+    const wine: string = component.getWineAsString();
+    expect(wine).toContain("TODO");
+  });
+
 });
--- hartappat/src/app/components/wine/wine.component.ts
+++ hartappat/src/app/components/wine/wine.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class WineComponent {
 
+  getWineAsString() {
+    return "TODO: replace me";
+  }
 }
