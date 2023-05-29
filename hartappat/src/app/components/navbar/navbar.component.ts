060afa6848f98775f835c986e4435608b6f5a729
Helge Stenström <h.stenstrom@gmail.com>
2023-05-29 20:26:01 +0200

    Plats i NavBar för att visa status. Just nu visas bara tiden.

--- hartappat/src/app/components/navbar/navbar.component.ts
+++ hartappat/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {interval, of, timestamp} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +11,18 @@ export class NavbarComponent  {
 
   // This class had both a constructor() and an ngOnInit(). Now removed.
 
+  protected readonly statusText = interval(1000)
+    .pipe(
+      map(() => this.getCurrentTime()),
+      map(t => 'Time and status indicator: ' + t)
+    );
+
+  private getCurrentTime(): string {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
 }
