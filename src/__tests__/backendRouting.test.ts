7eb6006ee7c1240f520b01801c8c69baf1670d34
Helge Stenström <h.stenstrom@gmail.com>
2024-03-30 00:38:17 +0100

    Fixar type warning TS2345.

--- src/__tests__/backendRouting.test.ts
+++ src/__tests__/backendRouting.test.ts
@@ -1,4 +1,7 @@
-import { app, setupEndpoints } from "../backendRouting";
+import { setupEndpoints } from "../backendRouting";
+import express, { Express, IRouterMatcher } from "express";
+
+// TODO: Klargör syftet med denna fil, eller ta bort den. Se till att den uppfyller sitt syfte.
 
 describe('from setupEndpoints', () => {
     test('postCountry', () => {
@@ -8,7 +11,8 @@ describe('from setupEndpoints', () => {
             get: () => console.log('GET was called'),
             patch: () => console.log('PATCH was called'),
             delete: () => console.log('DELETE was called'),
-        };
+        } as any as Express;  // From https://stackoverflow.com/questions/57964299/mocking-express-request-with-jest-and-typescript-using-correct-types
         setupEndpoints(router, {dialect: "sqlite",});
     });
 });
+
