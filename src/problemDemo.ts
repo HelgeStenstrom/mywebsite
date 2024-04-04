2f0b112aee88f674552f3122420cfa097a8bd5e1
Helge Stenström <h.stenstrom@gmail.com>
2024-04-04 23:22:34 +0200

    Uppstädning av exempelfil

--- src/problemDemo.ts
+++ src/problemDemo.ts
@@ -1,12 +1,8 @@
-import { Model, Optional } from "sequelize";
-
+import { Model } from "sequelize";
 
 interface SomeAttributes {
     id: number,
     name: string
 }
 
-interface   SomeValues extends Model<SomeAttributes>, SomeAttributes {}
-//interface  SomeInstance extends Sequelize.Instance<SomeAttributes>, SomeAttributes {}
-type  SomeCreationAttributes = Optional<SomeAttributes, 'id'>
-// funkar det nu?
+interface SomeValues extends Model<SomeAttributes>, SomeAttributes {}
