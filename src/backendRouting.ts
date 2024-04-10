e71a8681c4efcf8b574b724edfdf7f2599593081
Helge Stenström <h.stenstrom@gmail.com>
2024-04-11 00:55:16 +0200

    Refactoring: Flyttade funktioner från backendRouting.ts till endpointHandlers.ts
    Testat manuellt med GUI och Postman.

--- src/backendRouting.ts
+++ src/backendRouting.ts
@@ -13,15 +13,6 @@ function getConfiguredApp(): Express {
 
 export const app: Express = getConfiguredApp();
 
-export interface Member extends NodeJS.ReadableStream {
-    id: number,
-    givenName: string,
-    surname: string
-}
-
-interface Country extends NodeJS.ReadableStream {
-    name: string,
-}
 
 export function setupEndpoints(router: Express, sequelizeDbOptions) {
 
@@ -31,140 +22,25 @@ export function setupEndpoints(router: Express, sequelizeDbOptions) {
     const endpointHandlers = new EndpointHandlers(orm);
 
 
-    function getGrapes(): (req, res) => Promise<void> {
+    router.get('/api/v1/countries', endpointHandlers.getCountries());
+    router.post('/api/v1/countries', endpointHandlers.postCountries());
 
-        return async (req, res) => {
-            thenJson(orm.findGrapes(), res);
-        };
-    }
+    router.get('/api/v1/grapes', endpointHandlers.getGrapes());
+    router.post('/api/v1/grapes', endpointHandlers.postGrape());
+    router.patch('/api/v1/grapes', endpointHandlers.patchGrape());
 
-
-    function getCountries(): (req, res) => Promise<void> {
-
-        return async (req, res) => {
-            thenJson(orm.findCountries(), res);
-        };
-    }
-
-    function getAllTastings(): (req, res) => Promise<void> {
-
-        return async (req, res) => {
-            thenJson(orm.findTastings(), res);
-        };
-    }
-
-    function thenJson(promise: Promise<object[]>, res) {
-        promise
-            .then((x) => res.json(x))
-            .catch(e => console.error(e));
-    }
-
-    function getTasting(): (req, res) => Promise<void> {
-
-        return async (req, res) => {
-            console.log("get tasting with id=", req.params.id);
-            orm.getTasting(req.params.id)
-                .then((x) => res.json(x))
-                .catch(e => console.error(e));
-        };
-    }
-
-
-
-    function postGrape(): (req, res) => Promise<void> {
-
-        return async (req, res) => {
-
-            // We don't want to set the ID number; it should be handled by ORM.
-            const grape = {...(req.body), id: null}
-
-            orm.postGrape(grape)
-                .then((response) => res.status(201).json(response))
-                .catch(e => console.error(e));
-
-        };
-    }
-
-
-    function postCountries(): (req, res) => Promise<void>  {
-
-        return async (req, res) => {
-
-            const country: Country = req.body;
-
-            orm.postCountry(country)
-                .then(() => res.status(201).json("postCountries called!"))
-                .catch(e => console.error(e));
-
-        };
-    }
-
-    function deleteGrapeById() {
-        return async (req, res) => {
-            const id = req.params.id;
-
-            orm.delGrapeById(id)
-                .then((gnum) => {
-                    if (gnum)
-                        return res.status(204).json("Grape successfully deleted");
-                    else
-                        return res.status(404).json({ error: 'Grape not found' });
-                })
-                .catch(e => console.error(e));
-        };
-    }
-
-    function patchGrape(): (req, res) => Promise<void>  {
-
-        return async (req, res) => {
-            const {from, to} = req.body;
-
-            orm.patchGrapeByNameAndColor(from, to)
-                .then(() => res.status(200).json("patchGrape called!"))
-                .catch(e => console.error(e));
-        };
-    }
-
-    function getGrapeById() {
-
-        return async (req, res) => {
-            orm.getGrape(req.params.id)
-                .then((grape) => {
-                    if (grape)
-                        return res.status(200).json(grape);
-                    else
-                        return res.status(404).json({ error: 'Grape not found' });
-                })
-                .catch(e => console.error(e));
-            // TODO: Consider variant of return res.status(500).json({ error: 'Internal Server Error' })
-
-        };
-    }
-
-    router.get('/api/v1/countries', getCountries());
-    router.post('/api/v1/countries', postCountries());
-
-    router.get('/api/v1/grapes', getGrapes());
-    router.post('/api/v1/grapes', postGrape());
-    router.patch('/api/v1/grapes', patchGrape());
-
-    router.delete('/api/v1/grapes/:id', deleteGrapeById());
-    router.get('/api/v1/grapes/:id', getGrapeById());
+    router.delete('/api/v1/grapes/:id', endpointHandlers.deleteGrapeById());
+    router.get('/api/v1/grapes/:id', endpointHandlers.getGrapeById());
 
     router.get('/api/v1/members', endpointHandlers.getMembers());
     router.post('/api/v1/members', endpointHandlers.postMember());
 
-    router.get('/api/v1/tasting/:id', getTasting());
-    router.get('/api/v1/tasting/', getAllTastings());
+    router.get('/api/v1/tasting/:id', endpointHandlers.getTasting());
+    router.get('/api/v1/tasting/', endpointHandlers.getAllTastings());
 
     router.get('/api/v1/wines', endpointHandlers.getWines());
     router.post('/api/v1/wines', endpointHandlers.postWine());
 
     router.delete('/api/v1/wines/:id', endpointHandlers.deleteWineById());
 
-
-
 }
-
-
-
--- src/endpointHandlers.ts
+++ src/endpointHandlers.ts
@@ -2,14 +2,116 @@ import { Orm } from "./orm";
 import { Wine } from "../hartappat/src/app/services/backend.service";
 
 export class EndpointHandlers {
-    constructor(private orm: Orm) {}
+    constructor(private orm: Orm) {
+    }
 
-    getWines(): (req, res) => Promise<void> {
-        return async (req, res) => {
-            this.thenJson(this.orm.findWines(), res);
+// Common utilities
+    thenJson(promise: Promise<object[]>, res) {
+
+        return promise
+            .then((objects) => {
+                console.log('thenJson got a promise! ');
+                return res.status(200).json(objects);
+            })
+            .catch((err) => {
+                console.error('Got error', err);
+                res.status(503)
+                    .send("Ingen kontakt med databasen. Är Docker startad?");
+            });
+    }
+
+// Countries
+
+    postCountry(): (req, res) => void {
+        return function (p1, p2) {
+            throw Error("not implemented yet");
         };
     }
 
+    getCountries(): (req, res) => Promise<void> {
+
+        return async (req, res) => {
+            this.thenJson(this.orm.findCountries(), res);
+        };
+    }
+
+    postCountries(): (req, res) => Promise<void>  {
+
+        return async (req, res) => {
+
+            this.orm.postCountry(req.body)
+                .then(() => res.status(201).json("postCountries called!"))
+                .catch(e => console.error(e));
+
+        };
+    }
+
+
+// Grapes
+
+    getGrapes(): (req, res) => Promise<void> {
+
+        return async (req, res) => {
+            this.thenJson(this.orm.findGrapes(), res);
+        };
+    }
+
+    getGrapeById() {
+
+        return async (req, res) => {
+            this.orm.getGrape(req.params.id)
+                .then((grape) => {
+                    if (grape)
+                        return res.status(200).json(grape);
+                    else
+                        return res.status(404).json({ error: 'Grape not found' });
+                })
+                .catch(e => console.error(e));
+            // TODO: Consider variant of return res.status(500).json({ error: 'Internal Server Error' })
+
+        };
+    }
+
+    postGrape(): (req, res) => Promise<void> {
+
+        return async (req, res) => {
+
+            // We don't want to set the ID number; it should be handled by ORM.
+            const grape = {...(req.body), id: null}
+
+            this.orm.postGrape(grape)
+                .then((response) => res.status(201).json(response))
+                .catch(e => console.error(e));
+        };
+    }
+
+    patchGrape(): (req, res) => Promise<void>  {
+
+        return async (req, res) => {
+            const {from, to} = req.body;
+
+            this.orm.patchGrapeByNameAndColor(from, to)
+                .then(() => res.status(200).json("patchGrape called!"))
+                .catch(e => console.error(e));
+        };
+    }
+
+    deleteGrapeById() {
+        return async (req, res) => {
+            const id = req.params.id;
+
+            this.orm.delGrapeById(id)
+                .then((gnum) => {
+                    if (gnum)
+                        return res.status(204).json("Grape successfully deleted");
+                    else
+                        return res.status(404).json({ error: 'Grape not found' });
+                })
+                .catch(e => console.error(e));
+        };
+    }
+
+// Members
     getMembers(): (req, res) => Promise<void> {
         return async (_, res) => {
             this.thenJson(this.orm.findMembers(), res);
@@ -28,19 +130,33 @@ export class EndpointHandlers {
         };
     }
 
-    thenJson(promise: Promise<object[]>, res) {
+// Tastings
 
-        return promise
-            .then((objects) => {
-                console.log('thenJson got a promise! ');
-                return res.status(200).json(objects);
-            })
-            .catch((err) => {
-                console.error('Got error', err);
-                res.status(503)
-                    .send("Ingen kontakt med databasen. Är Docker startad?");
-            });
+    getTasting(): (req, res) => Promise<void> {
+
+        return async (req, res) => {
+            console.log("get tasting with id=", req.params.id);
+            this.orm.getTasting(req.params.id)
+                .then((x) => res.json(x))
+                .catch(e => console.error(e));
+        };
     }
+
+    getAllTastings(): (req, res) => Promise<void> {
+
+        return async (req, res) => {
+            this.thenJson(this.orm.findTastings(), res);
+        };
+    }
+
+// Wines
+
+    getWines(): (req, res) => Promise<void> {
+        return async (req, res) => {
+            this.thenJson(this.orm.findWines(), res);
+        };
+    }
+
 
     postWine() {
         return async (req, res) => {
@@ -48,22 +164,12 @@ export class EndpointHandlers {
 
             const wine: Wine = req.body;
 
-            const param = {name:wine.name, country:3, systembolaget:wine.systembolaget, volume:-1, winetype:2};
+            const param = {name: wine.name, country: 3, systembolaget: wine.systembolaget, volume: -1, winetype: 2};
             this.orm.postWine(param)
                 .then(() => res.status(201).json("postWine called!"))
                 .catch(e => console.error(e));
 
         };
-    }
-
-    postCountry(): (req, res) => void {
-        return function (p1, p2) {
-            throw Error("not implemented yet");
-        };
-    }
-
-    getCountries(): (req, res) => Promise<void>  {
-        throw Error("not implemented yet");
     }
 
     deleteWineById() {
@@ -75,10 +181,13 @@ export class EndpointHandlers {
                     if (gnum)
                         return res.status(204).json("Wine successfully deleted");
                     else
-                        return res.status(404).json({ error: 'Wine not found' });
+                        return res.status(404).json({error: 'Wine not found'});
                 })
                 .catch(e => console.error(e));
         };
 
     }
+
+
+
 }
