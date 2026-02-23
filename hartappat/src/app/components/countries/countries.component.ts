d00bea01517455056313cd76c62ab9b8b5ce41fb
Helge Stenström <h.stenstrom@gmail.com>
2026-02-23 17:01:48 +0100

    CountriesComponent listar länder.

--- hartappat/src/app/components/countries/countries.component.ts
+++ hartappat/src/app/components/countries/countries.component.ts
@@ -1,10 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BackendService, CountryApi} from "../../services/backend.service";
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css']
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit {
+
+  countries: CountryApi[] = [];
+
+  constructor(private backendService: BackendService) {}
+
+  ngOnInit(): void {
+    this.backendService.getCountries().subscribe(countries => {
+      this.countries = countries;
+      console.log(this.countries);
+      console.log("ngOnInit for CountriesComponent called.");
+    });
+  }
 
 }
