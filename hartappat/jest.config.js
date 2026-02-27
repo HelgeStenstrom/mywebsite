44aed49e4aba593f58c4d9261e9c497b32aac6cd
Helge Stenström <h.stenstrom@gmail.com>
2026-02-27 21:52:14 +0100

    Kan köra Jest för Angular frontend. Men de flesta tester är fail.

--- hartappat/jest.config.js
+++ hartappat/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['hartappat/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>//setup-jest.ts'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'ts-jest',
