2c8cd7dd21a84d206cc6faf22ab5c0c679e03f78
Helge Stenström <h.stenstrom@gmail.com>
2026-02-28 17:08:15 +0100

    Liten ändring i jest.config.js som får många tester att bli PASS.

--- hartappat/jest.config.js
+++ hartappat/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>//setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'ts-jest',
-  },
 };
