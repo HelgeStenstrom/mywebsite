314e1e4268dd37db5dbe2e8dd88c2fd62ac7e6f7
Helge Stenström <h.stenstrom@gmail.com>
2026-03-01 19:23:31 +0100

    Test av wine-types.component.spec.ts fungerar.

--- hartappat/src/app/components/wine-types/wine-types.component.spec.ts
+++ hartappat/src/app/components/wine-types/wine-types.component.spec.ts
@@ -1,14 +1,20 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {WineTypesComponent} from './wine-types.component';
+import {BackendService} from '../../services/backend.service';
+import {of} from 'rxjs';
 
 describe('WineTypesComponent', () => {
   let component: WineTypesComponent;
   let fixture: ComponentFixture<WineTypesComponent>;
 
+  const backendServiceMock = {
+    getWineTypes: () => of([]),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [WineTypesComponent]
+      declarations: [WineTypesComponent],
+      providers: [{provide: BackendService, useValue: backendServiceMock}]
     });
     fixture = TestBed.createComponent(WineTypesComponent);
     component = fixture.componentInstance;
