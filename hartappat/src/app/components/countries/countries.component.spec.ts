2f357adc5658e784af4e58f662c58e1689dbc86f
Helge Stenström <h.stenstrom@gmail.com>
2026-03-01 19:26:24 +0100

    Test av countries.component.spec.ts fungerar.

--- hartappat/src/app/components/countries/countries.component.spec.ts
+++ hartappat/src/app/components/countries/countries.component.spec.ts
@@ -1,14 +1,20 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {CountriesComponent} from './countries.component';
+import {BackendService} from '../../services/backend.service';
+import {of} from 'rxjs';
 
 describe('CountriesComponent', () => {
   let component: CountriesComponent;
   let fixture: ComponentFixture<CountriesComponent>;
 
+  const backendServiceMock = {
+    getCountries: () => of([]),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CountriesComponent]
+      declarations: [CountriesComponent],
+      providers: [{provide: BackendService, useValue: backendServiceMock}],
     });
     fixture = TestBed.createComponent(CountriesComponent);
     component = fixture.componentInstance;
