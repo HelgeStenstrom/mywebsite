1b4fe57aaa3f97a6d280980a0c9468098822d162
Helge Stenström <h.stenstrom@gmail.com>
2026-03-01 19:54:50 +0100

    Fick bort felutskrift i countries.component.spec.ts

--- hartappat/src/app/components/countries/countries.component.spec.ts
+++ hartappat/src/app/components/countries/countries.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {CountriesComponent} from './countries.component';
 import {BackendService} from '../../services/backend.service';
 import {of} from 'rxjs';
+import {FormsModule} from '@angular/forms';
 
 describe('CountriesComponent', () => {
   let component: CountriesComponent;
@@ -14,6 +15,7 @@ describe('CountriesComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CountriesComponent],
+      imports: [FormsModule],
       providers: [{provide: BackendService, useValue: backendServiceMock}],
     });
     fixture = TestBed.createComponent(CountriesComponent);
