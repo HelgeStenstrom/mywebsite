db9d435df057b378eca7ae9155d9c01e4527dacc
Helge Stenström <h.stenstrom@gmail.com>
2026-03-11 10:12:07 +0100

    Rättad test-setup för TastingComponent.

--- hartappat/src/app/components/tastings/tasting/tasting.component.spec.ts
+++ hartappat/src/app/components/tastings/tasting/tasting.component.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TastingComponent} from './tasting.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TastingComponent', () => {
   let component: TastingComponent;
@@ -8,9 +9,10 @@ describe('TastingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TastingComponent ]
+      declarations: [TastingComponent],
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TastingComponent);
     component = fixture.componentInstance;
