0505ef811c109cdca8374ad12b4403de5a95b9b5
Helge Stenström <h.stenstrom@gmail.com>
2026-03-14 22:29:55 +0100

    Förnamn på värdar syns där man visar vinprovningar.

--- hartappat/src/app/components/tastings/tasting/tasting.component.ts
+++ hartappat/src/app/components/tastings/tasting/tasting.component.ts
@@ -3,6 +3,7 @@ import {WineTasting} from "../../../models/tasting.model";
 import {Observable} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {TastingService} from "../../../services/backend/tasting.service";
+import {MemberService} from "../../../services/backend/member.service";
 
 @Component({
   selector: 'app-tasting',
@@ -13,10 +14,12 @@ export class TastingComponent implements OnInit {
 
   @Input() tasting?: WineTasting;
   fullTasting$!: Observable<WineTasting>;
+  memberNames: Map<number, string> = new Map();
 
 
   constructor(
     private readonly service: TastingService,
+    private readonly memberService: MemberService,
     private readonly route: ActivatedRoute,
   ) {
   }
@@ -24,6 +27,11 @@ export class TastingComponent implements OnInit {
   ngOnInit(): void {
     const id = this.tasting?.id ?? Number(this.route.snapshot.paramMap.get('id'));
     this.fullTasting$ = this.service.getTasting(id);
+
+    this.memberService.getMembers().subscribe(members => {
+      members.forEach(m => this.memberNames.set(m.id, m.given));
+    });
+
   }
 
 
