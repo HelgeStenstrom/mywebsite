65ee4c59a823cf7b5c3a236cd72d8b9656723621
Helge Stenström <h.stenstrom@gmail.com>
2026-03-30 19:08:53 +0200

    Vinmonopolet borttaget från HomeComponent.

--- hartappat/src/app/components/home/home.component.ts
+++ hartappat/src/app/components/home/home.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
-import {VinmonopoletComponent} from "../vinmonopolet/vinmonopolet.component";
 import {AboutComponent} from "../about/about.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
-  imports: [VinmonopoletComponent, AboutComponent],
+  imports: [AboutComponent],
 })
 export class HomeComponent {}
