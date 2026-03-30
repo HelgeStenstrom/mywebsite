6f91e42c2d4d2d96f5df658af97f430c2500423b
Helge Stenström <h.stenstrom@gmail.com>
2026-03-30 18:46:01 +0200

    Wikipedia och Vinmonopolet inlagda i testbench.component.html

--- hartappat/src/app/components/testbench/testbench.component.ts
+++ hartappat/src/app/components/testbench/testbench.component.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
+import {VinmonopoletComponent} from "../vinmonopolet/vinmonopolet.component";
+import {WikipediaComponent} from "../wikipedia/wikipedia.component";
 
 @Component({
-    selector: 'app-testbench',
-    templateUrl: './testbench.component.html',
-    styleUrls: ['./testbench.component.css'],
+  selector: 'app-testbench',
+  templateUrl: './testbench.component.html',
+  styleUrls: ['./testbench.component.css'],
+  imports: [
+    VinmonopoletComponent,
+    WikipediaComponent
+  ]
 })
 export class TestbenchComponent {
 }
