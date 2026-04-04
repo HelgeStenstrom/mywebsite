1f7b33edfde0508e8c971fb077e7e5d9465bf27b
Helge Stenström <h.stenstrom@gmail.com>
2026-04-04 14:05:18 +0200

    ScatterPlot skelett för plottar

---
+++ hartappat/src/app/components/scatter-plot/scatter-plot.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-scatter-plot',
+  imports: [],
+  templateUrl: './scatter-plot.component.html',
+  styleUrl: './scatter-plot.component.css',
+})
+export class ScatterPlotComponent {
+
+}
