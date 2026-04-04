b4a441abc5e8130ceb91e0f67c30ebd94591011a
Helge Stenström <h.stenstrom@gmail.com>
2026-04-04 12:08:35 +0200

    app.component.ts hämtar CurrentUser vid start, gör att frontend inte kan låtsas vara inloggad.

--- hartappat/src/app/app.component.spec.ts
+++ hartappat/src/app/app.component.spec.ts
@@ -1,34 +1,46 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {Component} from "@angular/core";
+import {of} from "rxjs";
+import {AuthService} from "./services/auth.service";
 
 describe('AppComponent', () => {
+  let authServiceMock: { fetchCurrentUser: jest.Mock };
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
+    authServiceMock = {
+      fetchCurrentUser: jest.fn().mockReturnValue(of(
+        { id: 1,
+          email: 'user@example.com',
+          memberId: null })),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent,
-      ],
+      declarations: [AppComponent,],
       imports: [MockNavbarComponent, MockRouterOutlet,],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+      ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'hartappat'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Härtappat');
+  it('should have as title "hartappat"', () => {
+    expect(component.title).toEqual('Härtappat');
   });
 
-
-  // Taking inspiration from
-  // https://www.digitalocean.com/community/tutorials/angular-introduction-unit-testing
-  // but my html is different, and the test is not really applicable.
-
+  test('calls fetchCurrentUser on init', () => {
+    expect(authServiceMock.fetchCurrentUser).toHaveBeenCalled();
+  });
 
 });
 
--- hartappat/src/app/app.component.ts
+++ hartappat/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
 @Component({
     selector: 'app-root',
@@ -6,6 +7,12 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css'],
     standalone: false
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Härtappat';
+
+  constructor(private readonly authService: AuthService,) { }
+
+  ngOnInit(): void {
+    this.authService.fetchCurrentUser().subscribe();
+    }
 }
